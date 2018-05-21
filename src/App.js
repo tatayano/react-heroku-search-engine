44469c19d64803ca77ea18f84acfeac8bacc5726
tatayano <tata.yano85@gmail.com>
2018-05-21 16:10:53 -0300

    Consome api externa

--- src/App.js
+++ src/App.js
@@ -1,51 +1,35 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) { 
+    super(props);
+    this.state = {
+      hits: [],
+    };
+  }
+
+  componentDidMount() { 
+    fetch('http://www.icarros.com.br/rest/search/v1/0/makes')
+      .then(response => response.json())
+      .then(data => this.setState({ hits: data }));
+  }
+
   render() {
+    const {hits} = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {hits.map(hit =>
+          <div key={hit.nome}>
+            {hit.nome}
+          </div>
+        )}
       </div>
     );
   }
